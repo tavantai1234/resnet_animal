2165a53f1f2ea0c5e92afad982f06098b4ab544c
Tavantai1234 <155410449+tavantai1234@users.noreply.github.com>
2026-05-13 09:53:04 +0700

    change cuda

--- trainresnet.py
+++ trainresnet.py
@@ -47,6 +47,14 @@ if __name__ == "__main__":
             if torch.backends.mps.is_available():
                 model = model.to("mps")
                 images, labels = images.to("mps"), labels.to("mps")
+            elif torch.cuda.is_available():
+                model = model.to("cuda")
+                images, labels = images.to("cuda"), labels.to("cuda")
+                
+            if torch.cuda.device_count() > 1:
+                print(print(f"Sử dụng {torch.cuda.device_count()} GPUs!"))
+                model = torch.nn.DataParallel(model) # Ép dùng cả 2 card T4
+                model.to('cuda')
 
             predict = model(images)
             print(predict)
