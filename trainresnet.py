ee8a8725cd4a8c619a7f9ca12900c7d22a08e523
Tavantai1234 <155410449+tavantai1234@users.noreply.github.com>
2026-05-13 09:28:32 +0700

    add cuda line 40

--- trainresnet.py
+++ trainresnet.py
@@ -37,6 +37,7 @@ if __name__ == "__main__":
 
     # checking mps
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logging.info("device: {}".format(device))
 
     # Training
